fd13c9b5c5d648fd5d8b5940a7c3d351f544d1e1
MeabhG97 <91346624+MeabhG97@users.noreply.github.com>
2023-11-20 16:24:18 +0000

    Added additional test for constructortree

--- test/ConstructTreeTests.cpp
+++ test/ConstructTreeTests.cpp
@@ -34,4 +34,33 @@ TEST(ConstructTreeTests, AddingDirNode) {
     d = dynamic_cast<Dir*>(root);
     EXPECT_EQ(d->children.size(), 1);
     EXPECT_EQ(d->children[0]->name, "child");
+
+    vs.clear();
+    vs.push_back("<dir>");
+    vs.push_back("root");
+    vs.push_back("<dir>");
+    vs.push_back("child0");
+    vs.push_back("<dir>");
+    vs.push_back("child");
+    vs.push_back("</dir>");
+    vs.push_back("</dir>");
+    vs.push_back("<dir>");
+    vs.push_back("child1");
+    vs.push_back("</dir>");
+    vs.push_back("</dir>");
+    root = ct.constructTree(vs);
+
+    ASSERT_NE(root, nullptr);
+    EXPECT_EQ(root->name, "root");
+    EXPECT_TRUE(dynamic_cast<Dir*>(root));
+
+    d = dynamic_cast<Dir*>(root);
+    EXPECT_EQ(d->children.size(), 2);
+    EXPECT_EQ(d->children[0]->name, "child0");
+    EXPECT_EQ(d->children[1]->name, "child1");
+
+    EXPECT_TRUE(dynamic_cast<Dir*>(d->children[0]));
+    d = dynamic_cast<Dir*>(d->children[0]);
+
+    EXPECT_EQ(d->children.size(), 1);
 }
